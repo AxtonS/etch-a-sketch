8c560e480614d23c3c425d4f1f45a5709b99b2b9
Axton Smith <axtonsmith@gmail.com>
2022-05-30 17:41:49 -0400

    Add hover function to grid

--- script.js
+++ script.js
@@ -6,14 +6,19 @@ const container = document.querySelector('#container');
 square.setAttribute('class', 'square'); */
 
 function buildGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
     const squares = 16 * 16;
     container.style.gridTemplate = 'repeat(16, 1fr) / repeat(16, 1fr)'
-    for(i = 256; i > 0; i--){
+    for(i = squares; i > 0; i--){
         const square = document.createElement('div');
         square.setAttribute('class', 'square');
+        square.addEventListener('mouseenter', function(event){
+            event.target.classList.add('hover');
+            });
         container.appendChild(square);
     }
 }
-
 const button = document.querySelector('button');
 button.addEventListener('click', buildGrid);

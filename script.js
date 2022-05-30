351b4c9944b445a125baa82bf63a2ca554d6f2ec
Axton Smith <axtonsmith@gmail.com>
2022-05-30 18:27:12 -0400

    Add random RGB

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 const container = document.querySelector('#container');
-/* container.style.gridTemplateColumns = '${}'
-    container.style.gridTemplateRows = '${}'
-*/
-/* const square = document.createElement('div');
-square.setAttribute('class', 'square'); */
-
 function buildGrid() {
     while (container.firstChild) {
         container.removeChild(container.firstChild);
@@ -13,12 +7,20 @@ function buildGrid() {
     if(squares > 100) {
         squares = 100;
     }
+    function randomColor() {
+    const r = 0 + Math.floor(Math.random() * (255));
+    const g = 0 + Math.floor(Math.random() * (255));
+    const b = 0 + Math.floor(Math.random() * (255));
+    return `rgb(${r},${g},${b})`;
+}
     container.style.gridTemplate = `repeat(${squares}, 1fr) / repeat(${squares}, 1fr)`
     for(i = squares * squares; i > 0; i--){
         const square = document.createElement('div');
         square.setAttribute('class', 'square');
         square.addEventListener('mouseenter', function(event){
-            event.target.classList.add('hover');
+            // event.target.classList.add('hover');
+            const color = randomColor();
+            event.target.style.backgroundColor = `${color}`;
             });
         container.appendChild(square);
     }

8d1369179090c53d67e556368791fe3498c3ee55
Axton Smith <axtonsmith@gmail.com>
2022-05-30 16:48:15 -0400

    Add buildGrid function

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ const container = document.querySelector('#container');
 /* container.style.gridTemplateColumns = '${}'
     container.style.gridTemplateRows = '${}'
 */
-const square = document.createElement('div');
-square.setAttribute('class', 'square');
+/* const square = document.createElement('div');
+square.setAttribute('class', 'square'); */
 
 function buildGrid() {
     const squares = 16 * 16;
     container.style.gridTemplate = 'repeat(16, 1fr) / repeat(16, 1fr)'
-    for(i = squares; i > 0; i--){
+    for(i = 256; i > 0; i--){
+        const square = document.createElement('div');
+        square.setAttribute('class', 'square');
         container.appendChild(square);
     }
 }
